41d5ddab6df5d379e43875c98f84b20b9004240d
user <user@users.noreply.github.com>
2015-05-15 10:16:00 +1200

    latestcode

--- public/js/model.js
+++ public/js/model.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-	$('.column1').click(function(){
-		$('.column1').last().css("background-color","red").removeClass('column1').addClass('new_class');
-});
-})
+var playerTurn = 'red';
+$(document).ready(function () {
+    $('.column1').click(doMove);
+}
+);
+
+function doMove() {
+    if (playerTurn == 'red') {
+        playerRed()
+    } else {
+        playerYellow()
+    };
+}
+
+function playerRed() {
+    $('.column1').last().css("background-color", "red").removeClass('column1').addClass('RedClass');
+    playerTurn = 'yellow';
+}
+
+
+function playerYellow() {
+
+    $('.column1').last().css("background-color", "yellow").removeClass('column1').addClass('YellowClass');
+    playerTurn = 'red';
+
+};
