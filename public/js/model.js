9b6aa904231153e93c2892526fb76a5df3e529d8
user <user@users.noreply.github.com>
2015-05-14 18:31:08 +1200

    updated

--- public/js/model.js
+++ public/js/model.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$('.column1').click(function(){
-		$('.column1').first().css("background-color","red");
+		$('.column1').last().css("background-color","red").removeClass('column1').addClass('new_class');
 });
-});
+})
