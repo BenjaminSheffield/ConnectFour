9b52012801feebbabf8c60fe715684788f5a6139
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-05-14 17:45:55 +1200

    AJAX get requests working

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -2,25 +2,33 @@ $(document).ready(function(){
 
   $.ajax({
     type: "GET",
-    dataType: "json",
-    url: "/api/winners",
+    url: "http://localhost:9393/api/winners",
     success: function(data){
       sortedScores = data.sort(compare);
-      console.log(sortedScores);
+      for (var i = 0; i < sortedScores.length; i++) {
+          var scores = sortedScores[i];
+          renderWinners(scores);
+        }
     },
     error: function(){
       console.log("Error");
     }
-
   });
-
 });
 
 function compare(a,b) {
-  if (a.win_count < b.win_count)
+  if (b.win_count < a.win_count)
      return -1;
-  if (a.win_count > b.win_count)
+  if (b.win_count > a.win_count)
     return 1;
   return 0;
 }
+
+function renderWinners(scores) {
+    var htmlString = "<li>" +
+                       "<h2>" + scores.name + "</h2>" +
+                       "<h3>" + scores.win_count + "</h3>" +
+                     "</li>";
+    $('#winners').append(htmlString);
+  }
 
