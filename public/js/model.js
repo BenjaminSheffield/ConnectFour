86b07156d8e06f83bd16eff8f27978b54f1ecc71
BenjaminSheffield & Shambu Ra Jowsey <pair+benjaminsheffield.samnz19@devbootcamp.com>
2015-05-15 15:47:50 +1200

    board showing array class names for individual cells

--- public/js/model.js
+++ public/js/model.js
@@ -1,18 +1,37 @@
 var playerTurn = "red";
 $(document).ready(function () {
+
     $('.Board').click(function (e) {
         var myClass = $(e.target).attr("class");
-        console.log(myClass);
+
         $('.' + myClass).last().css("background-color", playerTurn).removeClass(myClass).addClass('.' + playerTurn);
         if (playerTurn == "red") {
             playerTurn = "yellow"
         } else {
-            playerTurn = "red"
+            playerTurn = "red";
         }
-    }
+
+        board = [];
+        var rows = $('.Board').children().children();
+        storeRows = [];
+        for (var i = 0; i < rows.length; i++)
+        {
+            storeRows.push(rows[i].children);
+            array = []
+                for (var j = 0; j < 8; j++)
+                    {
+                        array.push(storeRows[i][j].className)
+                    }
+            board.push(array)
+        }
+        console.log(board)
+
+            }
     );
 }
 );
+
+
 
 
 
