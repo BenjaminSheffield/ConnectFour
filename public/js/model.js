2101caeb6a9e05e35eac2177f00d4da92a8d1fc6
Benjamin Sheffield <benjaminsheffield@live.com>
2015-05-16 15:43:34 +1200

    game win state

--- public/js/model.js
+++ public/js/model.js
@@ -1,4 +1,5 @@
 var playerTurn = "red";
+var winner;
 $(document).ready(function () {
 
     $('.Board').click(function (e) {
@@ -7,31 +8,102 @@ $(document).ready(function () {
         $('.' + myClass).last().css("background-color", playerTurn).removeClass(myClass).addClass('.' + playerTurn);
         if (playerTurn == "red") {
             playerTurn = "yellow"
+            winner = "Red";
         } else {
             playerTurn = "red";
+            winner = "Yellow";
         }
 
         board = [];
         var rows = $('.Board').children().children();
         storeRows = [];
-        for (var i = 0; i < rows.length; i++)
-        {
+        for (var i = 0; i < rows.length; i++) {
             storeRows.push(rows[i].children);
             array = []
-                for (var j = 0; j < 8; j++)
-                    {
-                        array.push(storeRows[i][j].className)
-                    }
+            for (var j = 0; j < 8; j++) {
+                array.push(storeRows[i][j].className)
+            }
             board.push(array)
         }
-        console.log(board)
 
-            }
+        setTimeout(checkRow, 10);
+        setTimeout(checkUpDiagonal, 10);
+        setTimeout(checkDownDiagonal, 10);
+        setTimeout(checkRedDown, 20);
+        setTimeout(checkYellowDown, 20);
+    }
     );
 }
 );
 
+function checkRedDown() {
+    var i;
+    for (m = 0; m < 8; m++) {
+        for (var k = 0; k < 8; k++) {
 
+            if (board[m][k] == (".red") && board[m][k] == board[(m - 1)][k] && board[m][k] == board[(m - 2)][k] && board[m][k] == board[(m - 3)][k]) {
+                GameEnds();
+                alert("Red wins!");
+            }
+        }
+    }
+}
+
+function checkYellowDown() {
+    var i;
+    for (m = 0; m < 8; m++) {
+        for (var k = 0; k < 8; k++) {
+
+            if (board[m][k] == (".yellow") && board[m][k] == board[(m - 1)][k] && board[m][k] == board[(m - 2)][k] && board[m][k] == board[(m - 3)][k]) {
+                GameEnds();
+                alert("Yellow wins!")
+            }
+        }
+    }
+}
+function checkRow() {
+    var i;
+    for (m = 0; m < 8; m++) {
+        for (var k = 0; k < 8; k++) {
+            if (board[m][k] == board[m][(k + 1)] && board[m][k] == board[m][(k + 2)] && board[m][k] == board[m][(k + 3)]) {
+                GameEnds();
+                alert(winner + " player wins!");
+            }
+        }
+    }
+}
+
+function checkDownDiagonal() {
+    var i;
+    for (m = 0; m < 8; m++) {
+        for (var k = 0; k < 8; k++) {
+
+            if (board[m][k] == board[(m + 1)][(k + 1)] && board[m][k] == board[(m + 2)][(k + 2)] && board[m][k] == board[(m + 3)][(k + 3)]) {
+                GameEnds();
+                alert(winner + " player wins!");
+            }
+        }
+    }
+}
+
+function checkUpDiagonal() {
+    var i;
+    for (m = 0; m < 8; m++) {
+        for (var k = 0; k < 8; k++) {
+
+            if (board[m][k] == board[(m + 1)][(k - 1)] && board[m][k] == board[(m + 2)][(k - 2)] && board[m][k] == board[(m + 3)][(k - 3)]) {
+                GameEnds();
+                alert(winner + " player wins!");
+            }
+        }
+    }
+
+}
+
+function GameEnds() {
+    var something = $('.Board').children().children().children().removeClass();
+    console.log(something);
+}
 
 
 
